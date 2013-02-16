957326e441de0fd12bc50d142859660058588f44
Daniel Hahn <dha@betterplace.org>
2013-02-16 16:10:30 +0100

    Fresh application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Introduction2013::Application.config.secret_token = '59696dd81ceb3cae5466b59432348511ddcd377275cdbe74f40670ff1e95e1045b443a633b2e8edcde2e1581f34978391e0417ce9a054d9912efdb110f5c4efc'
